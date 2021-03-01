caebfe4a8e6eb00695b1b13910e024f244f2b29e
mattarndt1 <60207898+mattarndt1@users.noreply.github.com>
2021-02-28 23:30:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@
       var boston_camera = new Camera({
         position: [
           -71.061434,
-          42.365693,
+          42.360693,
           9000// elevation in meters
         ],
         tilt: 0,
