467ce9788645df5bc1b1fd450ac6e6d88a9f2c3d
mattarndt1 <60207898+mattarndt1@users.noreply.github.com>
2021-02-28 23:16:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,114 @@
-var map = L.map('map').setView([37.85,-122.52], 14);
 
-  // load a tile layer
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 20,
-	ext: 'png'
-}).addTo(map);
+    require([
+      "esri/WebScene",
+      "esri/views/SceneView",
+      "esri/Camera",
+      "esri/widgets/Home",
+      "dojo/domReady!"
+    ], function(WebScene, SceneView, Camera, Home) {
 
-  // load GeoJSON from an external file
-  $.getJSON("https://github.com/mattarndt1/project_1--leaflet/blob/main/LCF_boundary.geojson",function(data){
-	  
-    // add GeoJSON layer to the map once the file is loaded
-    L.geoJson(data{	     
-	    "color": "#0000ff",
-	    "weight": 5
-	      }).addTo(map);
-	  
-/*
-	var ratIcon = L.icon({
-	    iconUrl: 'rat.gif',
-	    iconSize: [25,15]
-	  }); 
-	  L.geoJson(data  ,{
-	    pointToLayer: function(feature,latlng){
-		  return L.marker(latlng,{icon: ratIcon});
-	    }
-	  } );
-	  */
-  });
+    
+      /*var map = new Map({
+        basemap: "streets",
+        ground: "world-elevation"
+      });*/
+      var scene = new WebScene({
+        portalItem:{
+         id:"8046207c1c214b5587230f5e5f8efc77" 
+        }
+      });
+      
+      var boston_camera = new Camera({
+        position: [
+          -71.061434,
+          42.350693,
+          6000// elevation in meters
+        ],
+        tilt: 0,
+        heading: 0,
+        fov: 90
+      });
+      
+      var camera = new Camera({
+        position: [
+          -71.09777751,
+          42.34625234,
+          2// elevation in meters
+        ],
+        tilt: 90,
+        heading: 45
+      });
+      
+      var camera2 = new Camera({
+        position: {
+          x: -71.061179,
+          y: 42.377092,
+          z: 2
+        },
+        tilt: 90,
+        heading: 155
+      });
+      
+      var camera3 = new Camera({
+        position: {
+          x: -71.025577,
+          y: 42.355583,
+          z: 1000
+        },
+        tilt: 65,
+        heading: 277
+      });
+
+      var view =  new SceneView({
+        container: "viewDiv",
+        map: scene,
+        viewingMode:"global",
+        camera: boston_camera,
+        environment: {
+            lighting: {
+              date: new Date(),
+              directShadowsEnabled: true,
+              // don't update the view time when user pans.
+              // The clock widget drives the time
+              cameraTrackingEnabled: false
+            }
+        },
+    });
+      
+    var homeBtn = new Home({
+          view: view
+        });
+          
+      
+        view.ui.add(homeBtn, "top-left");
+    
+        [fen, bh, dwntn].forEach(function(button) {
+          button.style.display = 'flex';
+          view.ui.add(button, 'top-right');
+        });
+        
+      
+      
+    // bunker hill
+    bh.addEventListener('click', function() {
+    view.goTo({
+        target:camera2
+      });
+    });
+    
+    //fenway park
+    fen.addEventListener('click', function() {
+     view.goTo({
+        target:camera
+      });
+    });
+    
+      //downtown from SE
+    dwntn.addEventListener('click', function() {
+      view.goTo({
+        target:camera3
+      });
+    });
+
+
+    });
